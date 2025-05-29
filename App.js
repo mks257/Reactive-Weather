48a953690e6661e6d2045583cc37956e0fb9aa25
mks257 <138339735+mks257@users.noreply.github.com>
2025-05-29 12:05:33 -0500

    created form.js

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import WeatherCard from './components/WeatherCard';
-import Location from './components/location';
+import Location from './components/Location';
+import Form from './components/Form';
 import cities from './data';
 
 function App() {
-  const [location, setLocation] = useState("New York City"); // You can pick your default city
+  const [location, setLocation] = useState("New York City");
 
   return (
     <>
       <h1 className="title">REACTIVE WEATHER</h1>
       <h3 className="subtitle">Up to the minute weather news</h3>
+      <Form location={location} setLocation={setLocation} />
       <div className="app">
-        <Location data={cities} location={location} setLocation={setLocation} />
+        <Location data={cities} location={location} />
       </div>
     </>
   );
